263523d38d1f11c162555c6f8c184da94923ec23
BlackscorpionSK <160006077+BlackscorpionSK@users.noreply.github.com>
2026-02-02 05:53:08 +1000

    я устал

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,33 @@
+import org.w3c.dom.ls.LSInput;
+
+import java.util.Scanner;
+
 public class helloworld {
     public static void main(String[] args) {
-        System.out.println("hello");
-        System.out.println("world");
-        System.out.print("!");
+        /*System.out.println("hello\nworld!");
+
+        int age = 52;
+
+        System.out.println("age is " + age + " years");
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("input your name: ");
+        String name = scan.nextLine();
+        System.out.println("hello " + name);
+        System.out.println("Введите 1 число");
+        float num1 = scan.nextFloat();
+        System.out.println("Введите 2 число");
+        float num2 = scan.nextFloat();
+        float res1 = num1 + num2;
+        float res2 = num1 - num2;
+        float res3 = num1 * num2;
+        float res4 = num1 / num2;
+        System.out.println("результат ");
+        System.out.println(res1 + "\n" + res2 + "\n" + res3 + "\n" + res4);
+        */
+        short a = 3, b = 4;
+        if(a < b) {
+            System.out.println("java это ужас");
+        }
     }
 }
