098d01656b208a77a39e7e5d3dfba2c05c42a3e8
BlackscorpionSK <160006077+BlackscorpionSK@users.noreply.github.com>
2026-02-06 06:40:46 +1000

    1 лаба 2 задание

--- Lab_1_Stock.java
+++ Lab_1_Stock.java
@@ -1,2 +1,21 @@
-public class Lab_1_Stock {
+/*
+Напишите клиент этого класса — программу, которая создает объект типа Stock с
+обозначением SBER, именем ПАО Сбербанк, стоимостью акций на момент закрытия
+предыдущего дня, равной 281.50. Задайте новую стоимость акций в настоящий момент,
+равную 282.87, и отобразите процент изменения стоимости акций.
+*/
+public class Lab_1_Stock
+{
+    public static void main(String[] args)
+    {
+        Stock stock1 = new Stock("SBER","ПАО Сбербанк");
+        stock1.setPreviousClosingPrice(281.50);
+        stock1.setCurrentPrice(282.87);
+
+        System.out.println("обозначение акции: " + stock1.getSymbol());
+        System.out.println("имя: " + stock1.getName());
+        System.out.println("стоимость акции на момент закрытия предыдущего дня: " + stock1.getPreviousClosingPrice());
+        System.out.println("стоимость акций в настоящий момент: " + stock1.getCurrentPrice());
+        System.out.println("процент изменения стоимости акций: " + stock1.getChangePercent() + "%");
+    }
 }
--- Stock.java
+++ Stock.java
@@ -1,2 +1,60 @@
-public class Stock {
+/*
+    -Строковое поле данных с именем symbol для обозначения акций.
+    -Строковое поле данных с именем name для наименования акций.
+    -Поле данных previousClosingPrice типа double, в котором хранится стоимость акций
+на момент закрытия предыдущего дня.
+    -Поле данных currentPrice типа double, в котором хранится стоимость акций в
+настоящий момент.
+    -Конструктор, создающий акции с указанными обозначением и наименованием.
+    -Метод с именем getChangePercent(), который возвращает процент изменения
+стоимости акций с previousClosingPrice на currentPrice.
+*/
+public class Stock
+{
+    private String symbol;
+    private String name;
+    private double previousClosingPrice;
+    private double currentPrice;
+
+    public Stock(String symbol, String name)
+    {
+        this.symbol = symbol;
+        this.name = name;
+    }
+
+    public void setPreviousClosingPrice(double previousClosingPrice)
+    {
+        this.previousClosingPrice = previousClosingPrice;
+    }
+
+    public void setCurrentPrice(double currentPrice)
+    {
+        this.currentPrice = currentPrice;
+    }
+
+    public String getSymbol()
+    {
+        return this.symbol;
+    }
+
+    public String getName()
+    {
+        return this.name;
+    }
+
+    public double getPreviousClosingPrice()
+    {
+        return this.previousClosingPrice;
+    }
+
+    public double getCurrentPrice ()
+    {
+        return this.currentPrice;
+    }
+
+
+    public double getChangePercent()
+    {
+        return (currentPrice - previousClosingPrice) / previousClosingPrice * 100;
+    }
 }
